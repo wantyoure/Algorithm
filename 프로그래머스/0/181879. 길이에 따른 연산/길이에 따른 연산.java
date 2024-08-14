ee669d332e7229fbc849a4b71aa3f02752e2e0d2
SH_Park <qkrtmdgh55@naver.com>
2024-08-14 16:07:09 +0900

    [level 0] Title: 길이에 따른 연산, Time: 0.52 ms, Memory: 68.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        int result = 1;
+        for(int i=0; i<num_list.length; i++) {
+                if(num_list.length >= 11)  {
+                    answer += num_list[i];  
+                } else {
+                    result *= num_list[i];
+                    answer = result;
+                    System.out.print(answer);
+                }
+        }
+        return answer;
+    }
+}
